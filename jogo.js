f0f9fa3d94fbee46231b90a16d940c51bf2f33f6
YgorKronemberger <47404008+YgorKronemberger@users.noreply.github.com>
2019-07-02 17:56:53 -0300

    Add files via upload

---
+++ jogo.js
@@ -0,0 +1,138 @@
+var altura = 0
+var largura = 0
+var vidas = 1
+var tempo = 5
+
+var dificuldadeMosquito = 1500
+
+
+
+//--------Dificuldade ---------- > Continuar
+var nivel1 = window.location.search
+
+nivel1 = nivel1.replace('?', '')
+
+
+		alert(nivel1)
+
+
+
+
+
+
+//---------------------------------------
+
+function ajusta(){
+	altura = window.innerHeight
+	largura = window.innerWidth
+	//console.log(largura, altura)
+
+
+}
+
+
+ajusta()
+
+var cronometro = setInterval(
+	function(){
+	tempo -= 1
+	if(tempo < 0){
+		clearInterval(cronometro)
+		clearInterval(criar)
+		//alert("TEste")
+		window.location.href = 'Venceu.html'
+	}else{
+	document.getElementById('tempo').innerHTML = tempo
+	}
+
+}, 1000)
+
+	//document.getElementById('tempo').innerHTML = tempo
+
+//--------------------------------
+
+function pos(){
+
+	if(document.getElementById('mosquito'))
+
+	{
+
+		document.getElementById('mosquito').remove()
+
+			if (vidas > 3){
+
+				window.location.href = 'GameOver.html'
+
+			}
+				else{
+				document.getElementById('v' + vidas).src = 'imagens/coracao_vazio.png'
+
+				vidas++
+			}
+			
+
+	}
+
+
+
+var posiX = Math.floor(Math.random() * largura) - 90
+var posiY = Math.floor(Math.random() * altura) - 90
+
+	posiX = posiX < 0 ? 0 : posiX
+	posiY = posiY < 0 ? 0 : posiY
+
+
+console.log(posiX, posiY)
+
+var mosquito = document.createElement('img')
+mosquito.src = 'imagens/mosca.png'
+mosquito.className = tamanho() + ' ' + lado()
+mosquito.style.left = posiX + 'px'
+mosquito.style.top = posiY + 'px'
+mosquito.style.position = 'absolute'
+mosquito.id = 'mosquito'
+mosquito.onclick = function(){
+	this.remove()
+	//document.getElementById('mosquito').remove()
+}
+
+document.body.appendChild(mosquito)
+
+
+}
+
+//--------------------------------------
+
+function tamanho(){
+
+	var classe = Math.floor(Math.random() * 3)
+
+		switch(classe){
+
+			case 0:
+				return 'mosquito1'
+			case 1:
+				return 'mosquito2'
+			case 2:
+				return 'mosquito3'
+		}
+
+
+}
+
+//------------------------
+
+function lado(){
+
+	var classe = Math.floor(Math.random() * 2)
+
+			switch(classe){
+
+			case 0:
+				return 'ladoA'
+			case 1:
+				return 'ladoB'
+		}
+
+
+}
